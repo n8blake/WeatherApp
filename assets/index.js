149b2249e34d226a4f20b656a8dae9eb4fa8e247
Nathan Blake <developer@blake-ink.com>
2021-03-14 18:26:10 -0700

    Fix CitiesList, forecase, data api interface, layout updates

--- assets/index.js
+++ assets/index.js
@@ -1,31 +1,123 @@
+
+const citiesList = document.querySelector('#city-list-container');
+const cityErrorAlert = document.querySelector("#invalid-city-error");
+const cityListCaretIcon = document.querySelector("#collapse-caret-icon");
+
 const getCity = () => {
 	let query = document.querySelector("#city-input");
-	console.log(query.value);
+	//console.log(query.value);
 	weather.getCity(query.value).then(data => {
-		console.log(data);
-		setActiveCity(data);
+		//console.log(data);
+		if(data){
+			setActiveCity(data);
+			updateCityList(data.name);
+			dismissAlert();
+		} else {
+			showAlert();
+		}
 	});
 }
-let activeCity = null;
 
-const setActiveCity = (cityData) => {
+const showAlert = () => {
+	cityErrorAlert.style.display = "block";
+}
+
+const dismissAlert = () => {
+	cityErrorAlert.style.display = "none";	
+}
+
+citiesList.addEventListener('hide.bs.collapse', function(){
+	cityListCaretIcon.classList.remove('bi-caret-up-fill');
+	cityListCaretIcon.classList.add('bi-caret-down-fill');
+});
+
+citiesList.addEventListener('show.bs.collapse', function(){
+	cityListCaretIcon.classList.remove('bi-caret-down-fill');
+	cityListCaretIcon.classList.add('bi-caret-up-fill');
+});
+
+var cityListCollapse = new bootstrap.Collapse(citiesList, {
+  show: true
+})
+
+
+let activeCity = null;
+let activyCityForecastData = null;
+
+const setActiveCity = async (cityData) => {
+	//console.log(cityData);
 	activeCity = cityData;
 	document.querySelector("#active-city-title").textContent = cityData.name;
 	document.querySelector("#current-city-current-temp").textContent = temperatureString(cityData.main.temp);
-	document.querySelector("#active-city-weather-icon").innerHTML = '<img src="http://openweathermap.org/img/wn/' + cityData.weather[0].icon + '.png">';	
-	document.querySelector("#active-city-date").textContent = (new Date(cityData.dt)).toString();
+	document.querySelector("#active-city-weather-icon").innerHTML = '<img src="http://openweathermap.org/img/wn/' + cityData.weather[0].icon + '@2x.png">';	
+	document.querySelector("#active-city-date").textContent = longDateString(dateObject(cityData.dt));
+	document.querySelector("#active-city-high").textContent = "High: " + temperatureString(cityData.main.temp_max);
+	document.querySelector("#active-city-low").textContent = "Low: " + temperatureString(cityData.main.temp_min);
+	document.querySelector("#active-city-humidity").textContent = "Humidity: " + cityData.main.humidity + "%";
+	// get city forecase
+	activityCityForecastData = await weather.getCityForecast(cityData);
+	// for each day in the forecase, make a day card
+	const forecastContainer = document.querySelector("#forecast-container");
+	forecastContainer.innerHTML = "";
+	activityCityForecastData.daily.forEach(day => {
+		if(activityCityForecastData.daily.indexOf(day) === 0){
+			const uviElement = document.querySelector("#active-city-uvi");
+			uviElement.textContent = "UVI: " + day.uvi;
+			const uviClasses = ['uvi-low', 'uvi-moderate', 'uvi-high', 'uvi-very-high', 'uvi-extreme'];
+			uviClasses.forEach(uviClass => {uviElement.classList.remove(uviClass)});
+			if(day.uvi > 0 && day.uvi < 3){
+				// low - green
+				uviElement.classList.add(uviClasses[0]);
+			} else if (day.uvi >= 3 && day.uvi < 6){
+				// moderate - yellow
+				uviElement.classList.add(uviClasses[1]);
+			} else if(day.uvi >= 6 && day.uvi < 8){
+				// high - orange
+				uviElement.classList.add(uviClasses[2]);
+			} else if(day.uvi >= 8 && day.uvi < 11){
+				// very high - red
+				uviElement.classList.add(uviClasses[3]);
+			} else {
+				// extreme - purple
+				uviElement.classList.add(uviClasses[4]);
+			}
+		}
+		let card = makeDayCard(day);
+		forecastContainer.appendChild(card);
+	});
+	// update dom
 }
 
 // A Global Variable for changing temperature units
 var fahrenheit = true;
 
 const temperatureString = (kelvin) => {
-	//(0K − 273.15) × 
 	let temp = kelvin - 273.15;
 	if(fahrenheit){
 		temp = (temp * (9/5)) + 32;
 	} 
 	return Math.round(temp) + "°";
+}
+
+const dateObject = (utcSeconds) => {
+	let d = new Date(0);
+	d.setUTCSeconds(utcSeconds);
+	return d;
+}
+
+// Get Short Date string Mon 15 Mar
+const shortDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const shortDateString = (dateObject) => {
+	return shortDays[dateObject.getDay()] + ' ' + dateObject.getDate() + ' ' + shortMonths[dateObject.getMonth()];
+}
+
+// Get Long Date string  Monday 15 March, 2021
+const longDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const longMonths = ['January', 'February', 'March', 'April', 
+'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const longDateString = (dateObject) => {
+	return longDays[dateObject.getDay()] + ' ' + dateObject.getDate() + ' ' + longMonths[dateObject.getMonth()] + ', ' + dateObject.getFullYear();
 }
 
 const goBtn = document.querySelector("#getCityBtn");
@@ -45,13 +137,15 @@ const getWeatherIcon = (conditionCode, imgSrc) => {
 	return i;
 }
 
-const makeCityListItem = (name, condition, temp) => {
+const makeCityListItem = (name, index, condition, temp) => {
 	const cityListItem = document.createElement('li');
 	cityListItem.classList.add('list-group-item');
 	cityListItem.classList.add('city-list-item');
 	cityListItem.classList.add('d-flex');
 	cityListItem.classList.add('justify-content-between');
-	const cityName = document.createElement('span');
+	const cityName = document.createElement('h4');
+	cityName.classList.add('py-2');
+	cityName.classList.add('text-bold');
 	cityName.textContent = name;
 	cityListItem.appendChild(cityName);
 	const infoElement = document.createElement('span');
@@ -60,32 +154,27 @@ const makeCityListItem = (name, condition, temp) => {
 	temperatureElement.textContent = " " + temp;
 	infoElement.appendChild(temperatureElement);
 	cityListItem.appendChild(infoElement);
+	cityListItem.addEventListener('click', function(){
+		setActiveCity(citiesObjects[index]);
+	}, false);
 	return cityListItem;
 }
 
-let cities = [
-	{'name':'Everett',
-	'condition':'rain',
-	'temperature':61},
-	{'name':'Seattle',
-	'condition':'rain',
-	'temperature':60},
-	{'name':'San Francisco',
-	'condition':'cloudy',
-	'temperature':58},
-	{'name':'San Diego',
-	'condition':'rain',
-	'temperature':66},
-];
 
-const citiesList = document.querySelector('#city-list-container');
+
 citiesList.innerHTML = "";
 
-const initCities = async () =>{
+const initCities = async () => {
 	await cities.forEach( async function(city) {
-		const cityObject = await weather.getCity(city.name);
-		city.temperature = temperatureString(cityObject.main.temp);
-		let cityListItem = makeCityListItem(city.name, cityObject.weather[0].icon, city.temperature);
+		//console.log('getting city: ' + city);
+		const cityObject = await weather.getCity(city);
+		citiesObjects.push(cityObject);
+		const temperature = temperatureString(cityObject.main.temp);
+		let index = -1;
+		citiesObjects.forEach(c => {
+			if(c.name === city) index = citiesObjects.indexOf(c);
+		});
+		let cityListItem = makeCityListItem(city, index, cityObject.weather[0].icon, temperature);
 		citiesList.appendChild(cityListItem);
 		if(cities.indexOf(city) === 0){
 			//activeCity = cityObject;
@@ -94,18 +183,41 @@ const initCities = async () =>{
 	});
 }
 
-const addCaret = () => {
-	const historyToggleElement = document.createElement('li');
-	historyToggleElement.classList.add('list-group-item');
-	historyToggleElement.classList.add('d-flex');
-	historyToggleElement.classList.add('justify-content-center');
-	const caret = document.createElement('i');
-	caret.classList.add('bi');
-	caret.classList.add('bi-caret-down-fill');
-	historyToggleElement.appendChild(caret);
-	citiesList.appendChild(historyToggleElement);
+const makeDayCard = (dayData) => {
+	//console.log(dayData);
+	const col = document.createElement('div');
+	col.classList.add('col-6');
+	col.classList.add('col-md-3')
+	//col.classList.add('col-lg-3');
+	//col.classList.add('')
+	const card = document.createElement('div');
+	card.classList.add('card');
+	card.classList.add('forecast-card');
+	const cardHeader = document.createElement('div');
+	cardHeader.classList.add('card-header');
+	cardHeader.classList.add('forecast-header');	
+	cardHeader.textContent = (shortDateString(dateObject(dayData.dt)));
+	card.appendChild(cardHeader);
+	const cardBody = document.createElement('div');
+	const icon = document.createElement('h4');
+	icon.appendChild(getWeatherIcon(dayData.weather[0].icon, true));
+	//console.log(icon);
+	card.appendChild(icon);
+	const tempContainer = document.createElement('div');
+	const high = document.createElement('span');
+	high.textContent = temperatureString(dayData.temp.max);
+	const low = document.createElement('small');
+	low.textContent = " / " + temperatureString(dayData.temp.min);
+	low.classList.add('text-muted');
+	tempContainer.appendChild(high);
+	tempContainer.appendChild(low);
+	card.appendChild(tempContainer);
+	const humidityContainer = document.createElement('small');
+	humidityContainer.textContent = "Humidity: " + dayData.humidity + "%";
+	humidityContainer.classList.add('p-2');
+	card.appendChild(humidityContainer);
+	col.appendChild(card);
+	return col;
 }
-
-
 
 
--- assets/setup.js
+++ assets/setup.js
@@ -5,32 +5,84 @@ class WeatherHandler {
 	getCity(city){
 		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this._apiKey}`;
 		return fetch(url)
-			.then(response => response.json())
+			.then(response => {
+				//console.log(response);
+				if(response.ok){
+					return response.json();
+				} else {
+					return false;
+				}
+			})
 			.then(data => {
-				console.log(data);
+				//console.log(data);
 				return data;
 			});
 	}
 	getCityForecast(city){
-		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this._apiKey}`;
+		const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${this._apiKey}`;
 		return fetch(url)
 			.then(response => response.json())
 			.then(data => {
-				console.log(data);
 				return data;
 			});
 	}
 	set key(k){
-		console.log(k);
 		this._apiKey = k;
 	}
 }
 
 const weather = new WeatherHandler();
 
-fetch('http://localhost:9999/')
-	.then(response => response.json())
+fetch('https://blake-ink.com/WeatherAppAccess/')
+	.then(response => {
+		//console.log(response.json());
+		return response.json();
+	})
 	.then(data => {
 		weather.key = data;
-		initCities().then(addCaret());
+		initCities();
 	});
+
+
+// Place holder cities if none in local storage
+let demo_cities = ['Everett', 'Seattle', 'Washington', 'Dallas', 'San Diego'];
+
+// get history out of LocalStorage
+let citiesStr = localStorage.getItem('cities');
+var cities = [];
+var citiesObjects = [];
+if(citiesStr === null){
+	cities = demo_cities;
+} else {
+	cities = JSON.parse(citiesStr);
+
+}
+
+const updateCityList = async (city) => {
+	if(cities.indexOf(city) === -1){
+		//console.log("adding: " + city);
+		cities.push(city);
+		const cityObject = await weather.getCity(city);
+		citiesObjects.push(cityObject);
+		const temperature = temperatureString(cityObject.main.temp);
+		let index = -1;
+		citiesObjects.forEach(c => {
+			if(c.name === city) index = citiesObjects.indexOf(c);
+		});
+		let cityListItem = makeCityListItem(city, index, cityObject.weather[0].icon, temperature);
+		citiesList.appendChild(cityListItem);
+		saveCities();
+	}
+}
+
+const saveCities = () => {
+	let citiesStr = JSON.stringify(cities);
+	localStorage.setItem('cities', citiesStr);
+}
+
+const removeActiveCityFromCitiesList = () => {
+	cities.splice(cities.indexOf(activeCity.name), 1);
+	saveCities();
+	citiesList.innerHTML = "";
+	initCities();
+}
